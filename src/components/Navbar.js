020103c38a034b324892040424f56b99d7a53a66
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-20 14:20:33 -0300

    adicionando componente de navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import styles from "./Navbar.module.css";
+
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <NavLink to="/">
+        Mini <span>Blog</span>
+      </NavLink>
+      <ul>
+        <li>
+          <NavLink to="/"> Home </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about"> About </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
