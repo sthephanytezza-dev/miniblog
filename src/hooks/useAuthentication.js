46f42331526bb5fd240f941c3ec5765f9f154be0
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-20 15:48:14 -0300

    contexto da autenticação

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -62,45 +62,6 @@ export const useAuthentication = () => {
     }
   };
 
-  const logout = () => {
-    checkIfIsCancelled();
-
-    signOut(auth);
-  };
-
-  const login = async (data) => {
-    checkIfIsCancelled();
-
-    setLoading(true);
-    setError(false);
-
-    try {
-      await signInWithEmailAndPassword(auth, data.email, data.password);
-    } catch (error) {
-      console.log(error.message);
-      console.log(typeof error.message);
-      console.log(error.message.includes("user-not"));
-
-      let systemErrorMessage;
-
-      if (error.message.includes("user-not-found")) {
-        systemErrorMessage = "Usuário não encontrado.";
-      } else if (error.message.includes("wrong-password")) {
-        systemErrorMessage = "Senha incorreta.";
-      } else {
-        systemErrorMessage = "Ocorreu um erro, por favor tenta mais tarde.";
-      }
-
-      console.log(systemErrorMessage);
-
-      setError(systemErrorMessage);
-    }
-
-    console.log(error);
-
-    setLoading(false);
-  };
-
   useEffect(() => {
     return () => setCancelled(true);
   }, []);
@@ -109,8 +70,6 @@ export const useAuthentication = () => {
     auth,
     createUser,
     error,
-    logout,
-    login,
     loading,
   };
 };
