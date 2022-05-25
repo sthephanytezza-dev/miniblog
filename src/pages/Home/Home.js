f1ba8821e2199f1989f6f34f814de679ac979f46
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-25 17:51:56 -0300

    estruturando a home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,37 @@
 import styles from "./Home.module.css";
 
+//hooks
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+
+// components
+
 const Home = () => {
+  const [query, setQuery] = useState("");
+  const [posts] = useState([]);
+
+  const handleSubmit = () => {};
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>Veja nossos posts mais recentes:</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input
+          type="text"
+          placeholder="Ou busque por tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <h1>Posts...</h1>
+      {posts && posts.length === 0 && (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts</p>
+          <Link to="/posts/create" className="btn">
+            Criar novo post
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
