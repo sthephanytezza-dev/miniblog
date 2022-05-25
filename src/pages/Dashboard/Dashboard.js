664f008e78723fc6a8e3b56892eb2a79463c66bc
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-25 19:34:03 -0300

    exibindo posts do usuário na dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -11,7 +11,7 @@ const Dashboard = () => {
   const uid = user.id;
 
   //posts do usuário
-  const posts = [];
+  const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
 
   return (
     <div>
@@ -20,11 +20,15 @@ const Dashboard = () => {
       {posts && posts.length === 0 ? (
         <div className={styles.noposts}>
           <p>Não foram encontrados posts</p>
-          <Link to="/posts/create" className="btn">Criar primeiro post</Link>
+          <Link to="/posts/create" className="btn">
+            Criar primeiro post
+          </Link>
         </div>
       ) : (
         <div>Tem posts!</div>
       )}
+
+      {posts && posts.map((post) => <h3>{post.title}</h3>)}
     </div>
   );
 };
