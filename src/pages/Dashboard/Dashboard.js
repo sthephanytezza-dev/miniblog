aa8858b24ebdff2b52f900538cf5eae67ba5f87e
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-25 19:49:41 -0300

    página dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -13,8 +13,14 @@ const Dashboard = () => {
   //posts do usuário
   const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
 
+  const deleteDocument = (id) => {};
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
   return (
-    <div>
+    <div className={styles.dashboard}>
       <h2>Dashboard</h2>
       <p>Gerencie os seus posts</p>
       {posts && posts.length === 0 ? (
@@ -25,10 +31,32 @@ const Dashboard = () => {
           </Link>
         </div>
       ) : (
-        <div>Tem posts!</div>
+        <div className={styles.post_header}>
+          <span>Título</span>
+          <span>Ações</span>
+        </div>
       )}
 
-      {posts && posts.map((post) => <h3>{post.title}</h3>)}
+      {posts &&
+        posts.map((post) => (
+          <div className={styles.post_row} key={post.id}>
+            <p>{post.title}</p>
+            <div className={styles.actions}>
+              <Link to={`/posts/${post.id}`} className="btn btn-outline">
+                Ver
+              </Link>
+              <Link to={`/posts/edit/${post.id}`} className="btn btn-outline">
+                Editar
+              </Link>
+              <button
+                onClick={() => deleteDocument(post.id)}
+                className="btn btn-outline btn-danger"
+              >
+                Excluir
+              </button>
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
